9c4a3cfe16d2c76db17f4f2cec56e4ada8067084
bermarte <bermarte@hotmail.com>
2020-05-08 21:00:13 +0200

    trying cleaning up

--- Cordova/games/platforms/browser/www/js/index.js
+++ Cordova/games/platforms/browser/www/js/index.js
@@ -5,7 +5,6 @@ document.addEventListener("deviceready", toestel_klaar);
 function toestel_klaar() {
     
     document.getElementById('toestel_info').addEventListener('click', device_info);
-    //document.getElementById('toestel_info').addEventListener('click', device_info);
     //document.getElementById('toestel_info').addEventListener('click', onBatteryStatus);
 
     let boodschap = document.createElement('p');
@@ -17,7 +16,7 @@ function toestel_klaar() {
         let device_model = device.model;
         let device_platform = device.platform;
 
-        boodschap.innerHTML = 'uw model is: '+device_model+' en u platform is '+device_platform;
+        boodschap.innerHTML = 'your device is: '+device_model+' and your platform is '+device_platform;
         document.getElementById('info_weergeven').appendChild(boodschap);
 
     }
@@ -31,7 +30,6 @@ function toestel_klaar() {
         document.getElementById('info_battery').appendChild(boodschap);
         
     }
-
 
 }
 
