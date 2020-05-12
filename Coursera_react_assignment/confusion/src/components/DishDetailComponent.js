b0a7b2ac7e0f219b204c4f03d7324507ad8d1bad
bermarte <bermarte@hotmail.com>
2020-05-12 14:42:16 +0200

    moving map function inside a function

--- Coursera_react_assignment/confusion/src/components/DishDetailComponent.js
+++ Coursera_react_assignment/confusion/src/components/DishDetailComponent.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component{
-
+   
     render() {
             const months =["January","February","March","April","May","June","July","August","September","October","November","December"];
+            
             function giveMonth(num){
                 return months[num-1];
+            }
+
+            function renderComments(arr) {
+                if (arr.length < 0 || typeof arr === 'undefined') 
+                    return <div></div>; 
+                else {
+                    return (arr.map((comm, i) =>  
+                        <li key={i} className='col pb-4'>{comm.comment}
+                            <div className='pt-4'> -- {comm.author}, {giveMonth(Number(comm.date.substring(5,7)))} 
+                            {comm.date.substring(8,10)}, {comm.date.substring(0,4)} 
+                            </div>
+                        </li>)
+                     );
+                }
             }
 
             if (this.props.dish != null){
@@ -27,11 +42,7 @@ class DishDetail extends Component{
                                 <h4>Comments</h4>
                                 <div className="row">
                                 <ul className="list-unstyled">
-                                    {this.props.dish.comments.map((comm, i) =>  
-                                                                                <li key={i} className='col pb-4'>{comm.comment}
-                                                                                    <div className='pt-4'> -- {comm.author}, { giveMonth(Number(comm.date.substring(5,7)))} {comm.date.substring(8,10)}, {comm.date.substring(0,4)} </div>
-                                                                                </li>)
-                                    }
+                                    {renderComments(this.props.dish.comments)}
                                 </ul>
                             </div>
                             </div>
