0984cd1663ea12f51b1c5d1d5250af9807c179b4
bermarte <bernardo.martelli@gmail.com>
2020-05-27 11:56:55 +0200

    working on canvas

--- Cordova/games/platforms/android/app/src/main/assets/www/21/js/main.js
+++ Cordova/games/platforms/android/app/src/main/assets/www/21/js/main.js
@@ -4,17 +4,35 @@ window.addEventListener("load", function () {
     const c_black = document.getElementById("canvas_black");
     const ctx_black = c_black.getContext("2d");
 
-    //c_black.width = 300;
-    //c_black.height = 500;
-});
+    c_black.width = 439;
+    c_black.height = 200;
+    //background
+    ctx_black.fillStyle = "#d0bfff";
+    ctx_black.fillRect(0, 0, c_black.width, c_black.height);
+    //two stages
+    ctx_black.beginPath();
+    ctx_black.moveTo(0, c_black.height/2);
+    ctx_black.lineTo(c_black.width, c_black.height/2);
+    ctx_black.stroke();
+
 
 function BlackJack() {
     let score = 0; //default 0
     let dealer_score = 0; //default 0
+    let you = [];
+    let enemy = [];
     const MIN = 1;
     const MAX = 11;
     const BJ_MAX = 21;
     const MAX_DEALER = 15;
+    const square = 50;
+    //canvas
+    let pos_x_you = 0;
+    let pos_y_you = 25;
+    const spacer = 20;
+
+    let pos_x_enemy = 0;
+    let pos_y_enemy = 25;
 
     Object.defineProperty(this, 'surrender', {
         get: function () {
@@ -37,9 +55,20 @@ function BlackJack() {
         let hit = randomInt(MIN, MAX);
         console.log(`we have a ${hit}`);
         score += hit;
+        if (pos_x_you == 0)pos_x_you += spacer;
+        else{
+            pos_x_you += square+spacer;
+        }   
+        you.push(hit);
+
+        ctx_black.beginPath();
+        ctx_black.rect(pos_x_you, pos_y_you, square, square);
+        ctx_black.stroke();
+
+        console.log(`your points: ${you}`);
         console.log(`our score is ${score}`);
         document.getElementById('black_score').innerHTML = score;
-        let res = document.getElementById('black_results');
+        //let res = document.getElementById('black_results');
         if (score > BJ_MAX) {
             console.log("you loose");
             surrender();
@@ -64,6 +93,7 @@ function BlackJack() {
         console.log('play again');
         score = 0;
         dealer_score = 0;
+        pos_x_you = 0;//canvas
         document.getElementById('black_dealer_score').innerHTML = '?';
         document.getElementById('black_score').innerHTML = score;
         black_div.innerHTML = "<input type='button' class='btn btn-primary' value='HIT' id='hit'><input type='button' class='btn btn-primary' value='STAND' id='stand'><input type='button' class='btn btn-primary' value='SURRENDER' id='surrender'>";
@@ -74,7 +104,7 @@ function BlackJack() {
     }
     let feedback = null;
     let win = function () {
-        msg.innerHTML = '<h4 class="bg-dark text-white my-5">You Win</h4>';
+        msg.innerHTML = '<h4 class="bg-dark text-white">You Win</h4>';
         results.appendChild(msg);
         black_div.innerHTML = "<input type='button' class='btn btn-primary' value='PLAY AGAIN' id='play_again'>";
         document.getElementById('play_again').addEventListener('click', replay);
@@ -97,12 +127,14 @@ function BlackJack() {
         results.appendChild(msg);
         black_div.innerHTML = "<input type='button' class='btn btn-primary' value='PLAY AGAIN' id='play_again'>";
         document.getElementById('play_again').addEventListener('click', replay);
-        feedback = 'tie';
+        feedback = 'Tie';
         msg_box();
     }
 
     function msg_box() {
         if (confirm(feedback)) {
+            you = [];
+            enemy = [];
             return;
           }
     }
@@ -124,6 +156,8 @@ function BlackJack() {
                 let hit_dealer = randomInt(MIN, MAX);
                 console.log(`dealer has a ${hit_dealer}`);
                 dealer_score += hit_dealer;
+                enemy.push(hit_dealer);
+                console.log(`points of enemy: ${enemy}`);
                 console.log(`dealer's score is ${dealer_score}`);
             }
             while (
@@ -168,3 +202,6 @@ black_game.score = 7;
 console.log(black_game.score);
 black_game.hit();
 */
+
+
+});
