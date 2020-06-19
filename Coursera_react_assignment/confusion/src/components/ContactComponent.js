ea240478ce3fc0e276cdab58c132c1f92490c2ad
bermarte <bernardo.martelli@gmail.com>
2020-06-19 12:37:41 +0200

    React Router and Single Page Applications

--- Coursera_react_assignment/confusion/src/components/ContactComponent.js
+++ Coursera_react_assignment/confusion/src/components/ContactComponent.js
@@ -15,6 +15,7 @@ function Contact(props) {
                     <hr />
                 </div>                
             </div>
+            
             <div className="row row-content">
                 <div className="col-12">
                 <h3>Location Information</h3>
--- Coursera_react_assignment/confusion/src/components/MainComponent.js
+++ Coursera_react_assignment/confusion/src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -50,6 +51,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           {/* if the route does not match anyone of these Redirects */}
           <Redirect to="/home" />
         </Switch>
