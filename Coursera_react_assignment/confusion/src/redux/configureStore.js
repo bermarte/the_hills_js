e7de2e23facf59bb25e83ade6017e5a5ccfa71cd
bermarte <bernardo.martelli@gmail.com>
2020-08-04 00:25:01 +0200

    combining reducers

--- Coursera_react_assignment/confusion/src/redux/configureStore.js
+++ Coursera_react_assignment/confusion/src/redux/configureStore.js
@@ -1,10 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
 
 export const configureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
     return store;
 }
